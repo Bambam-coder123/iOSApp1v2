c2ab79699b23908df9396082b2c4a3e2bdb604aa
Bambam-coder123 <virgilholder23@gmail.com>
2025-09-18 10:58:59 -0400

    Added order view

--- iOSApp1v2/ContentView.swift
+++ iOSApp1v2/ContentView.swift
@@ -5,3 +5,32 @@
 //  Created by Mac User on 2025-09-18.
 //
 
+import SwiftUI
+
+struct OrderView: View {
+    let menuItems = ["Double Double", "Iced Capp", "Bagel with Cream Cheese", "Timbits"]
+    let prices = [2.19, 3.49, 1.99, 2.99]
+    
+    let index: Int
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Text(menuItems[index])
+                    .font(.title)
+                Text(String(format: "$%.2f", prices[index]))
+                    .font(.subheadline)
+            }
+            
+            Text("Customization Options")
+            Text("Save Order Button")
+            Text("Order History Button")
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView(index: 0)
+    }
+}
